505f035edbb5ea89509c08cdbbeb4428da6ad4d1
Ven Kim <venvex@gmail.com>
2021-11-14 00:52:06 -0500

    created tag update and delete endpoints

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,9 +11,8 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
         through: ProductTag,
-        as: "products",
+        as: "tagged_product",
       },
     ],
   })
@@ -28,23 +27,20 @@ router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   Tag.findOne({
-    where: {
+    where: { 
       id: req.params.id,
     },
     include: [
       {
         model: Product,
-        attributes: ["id", "product_name", "price", "stock", "category_id"],
         through: ProductTag,
-        as: "products",
+        as: "tagged_product",
       },
     ],
   })
     .then((dbTagData) => {
       if (!dbTagData) {
-        res
-          .status(404)
-          .json({ message: "There was no tag found with this id." });
+        res.status(404).json({ message: "There was no tag found with this id!" });
         return;
       }
       res.json(dbTagData);
@@ -69,10 +65,46 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "There was no tag found with this id!" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "There was no tag found with this id!" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log((err) => {
+        res.status(500).json(err);
+      });
+    });
 });
 
 module.exports = router;
