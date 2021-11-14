32ad98a5e1a52f310e57e3f3cdcd3360a775b532
Ven Kim <venvex@gmail.com>
2021-11-14 00:01:44 -0500

    created category update and destory routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -56,7 +56,7 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name,
   })
-    .then(dbCategoryData) => res.json(dbCategoryData)
+    .then((dbCategoryData) => res.json(dbCategoryData))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -65,10 +65,38 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  ).then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: "No category found!" });
+      return;
+    }
+    res.json(dbCategoryData);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found!" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
